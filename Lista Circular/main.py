beb821fd0f47d2ceaa5968e31e8c0e1e96dd2234
AleVargas10 <l413vc@gmail.com>
2024-06-07 21:02:06 -0600

    Add files via upload

---
+++ Lista Circular/main.py
@@ -0,0 +1,39 @@
+from ListaCircular import ListaCircular
+miLista = ListaCircular()
+numero = 0
+opcion = 0
+
+miLista.agregar(1)
+miLista.agregar(2)
+miLista.agregar(3)
+
+while opcion != 6:
+    opcion = int(input("\n1.Ingresar\n2.Mostrar\n3.Buscar\n4.Modificar\n5.Eliminar\n6.Salir\nDigite la opcion que require realizar:"))
+    
+    match opcion:
+        case 1:
+            numero = int(input("Digite el número que desea insertar: "))
+            miLista.agregar(numero)
+            
+        case 2:
+            print(miLista.mostrar())
+            
+        case 3:
+            numero= int(input("Digite el número que desea buscar: "))
+            if (miLista.buscar(numero)):
+                print("Número encontrado.")
+            else:
+                print("Número no encontrado.")
+                
+        case 4:
+            miLista.modificar()
+        
+        case 5:
+            numero = int(input("Digite el numero que desea eliminar: "))
+            miLista.eliminar(numero)
+            
+        case 6:
+            print("Saliendo del programa...")
+
+        case _:
+            print("Opcion invalida.")
